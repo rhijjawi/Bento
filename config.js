e701fe6199fe687936bb76043f8bf7d341065cf2
rhijjawi <18570300+rhijjawi@users.noreply.github.com>
2022-02-07 08:56:57 +0300

    Update config.js

--- config.js
+++ config.js
@@ -28,9 +28,9 @@ const CONFIG = {
   weatherUnit: 'C', // 'F', 'C'
   language: 'en', // More languages in https://openweathermap.org/current#multi
 
-  trackLocation: true, // If false or an error occurs, the app will use the lat/lon below
-  defaultLatitude: '37.775',
-  defaultLongitude: '-122.419',
+  trackLocation: false, // If false or an error occurs, the app will use the lat/lon below
+  defaultLatitude: '0.2295',
+  defaultLongitude: '32.5643',
 
   // Autochange
   autoChangeTheme: true,
