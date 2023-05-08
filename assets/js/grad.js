621625d0b8e581348159835e2e33550b0f86dea8
Ramzi H <18570300+rhijjawi@users.noreply.github.com>
2023-05-08 21:48:06 +0300

    Update grad.js

--- assets/js/grad.js
+++ assets/js/grad.js
@@ -1,5 +1,5 @@
 let _then;
-
+var buttonCounter = 0;
 var _msecond = 1;
 var _second = 1000;
 var _minute = _second * 60;
@@ -16,4 +16,22 @@ const GRADCOUNTER = () => {
   var miliseconds = Math.floor((distance % _second) / _msecond);
   document.querySelector('#grad').innerHTML = `${days} days, ${hours} hours, ${minutes} minutes, ${seconds} seconds, and ${miliseconds} miliseconds left until you graduate!`
 };
+const emCounter = ()=> {
+ if (buttonCounter > 2){
+  _thenEm = new Date('06/14/2023 08:35:00').getTime();
+  distanceEm = _thenEm - new Date();
+  var daysEm = Math.floor(distanceEm / _day);
+  var hoursEm = Math.floor((distanceEm % _day) / _hour);
+  var minutesEm = Math.floor((distanceEm % _hour) / _minute);
+  var secondsEm = Math.floor((distanceEm % _minute) / _second);
+  var milisecondsEm = Math.floor((distanceEm % _second) / _msecond);
+  document.querySelector('#EmCounter').innerHTML = `${daysEm} days, ${hoursEm} hours, ${minutesEm} minutes, ${secondsEm} seconds, and ${milisecondsEm} miliseconds left until you graduate!`
+ }
+ else {
+  document.querySelector('#EmCounter').innerHTML = ``;
+ }
+ 
+}
 setInterval(GRADCOUNTER, 10);
+setInterval(emCounter, 10);
+document.getElementById('emelyButton').onclick = (()=>{buttonCounter++}) 
